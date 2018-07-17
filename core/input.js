796c398a0b3e18950b77d97190b5f579fceae5d9
satanas <wil.alejandro@gmail.com>
2018-07-16 23:24:56 -0700

    Added input module

---
+++ core/input.js
@@ -0,0 +1,40 @@
+class Input {
+  constructor() {
+    this.pressed = {};
+    this.events = {};
+
+    D.body.addEventListener('keydown', (e) => {
+      if (e.keyCode in this.pressed) {
+        e.preventDefault();
+        this.events[e.keyCode] = -1;
+        this.pressed[e.keyCode] = 1;
+      }
+    });
+
+    D.body.addEventListener('keyup', (e) => {
+      if (e.keyCode in this.pressed) {
+        e.preventDefault();
+        this.events[e.keyCode] = 1;
+        this.pressed[e.keyCode] = 0;
+      }
+    });
+  }
+
+  isPressed(c) {
+    if (c instanceof Array) {
+      for(let i = 0; i < c.length; i++) {
+        if (!!this.pressed[c[i]]) return true;
+      }
+      return false;
+    } else {
+      return !!this.pressed[c];
+    }
+  }
+
+  bind(keys) {
+    keys.forEach((k) => {
+      this.pressed[k] = 0;
+      this.events[k] = 0;
+    });
+  }
+}
