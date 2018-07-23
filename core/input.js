813fc711091820e216f9f944cdf8ba25683fad2d
satanas <wil.alejandro@gmail.com>
2018-07-22 18:20:24 -0700

    Adding mouse support to input module

--- core/input.js
+++ core/input.js
@@ -4,18 +4,28 @@ class Input {
     this.events = {};
 
     D.body.addEventListener('keydown', (e) => {
-      if (e.keyCode in this.pressed) {
-        e.preventDefault();
-        this.events[e.keyCode] = -1;
-        this.pressed[e.keyCode] = 1;
+      if (e.keyCode in this.events) {
+        this.updateEvent(e, -1, 1);
       }
     });
 
     D.body.addEventListener('keyup', (e) => {
-      if (e.keyCode in this.pressed) {
-        e.preventDefault();
-        this.events[e.keyCode] = 1;
-        this.pressed[e.keyCode] = 0;
+      if (e.keyCode in this.events) {
+        this.updateEvent(e, 1, 0);
+      }
+    });
+
+    D.body.addEventListener('mousedown', (e) => {
+      let key = e.button + 1;
+      if (key in this.events) {
+        this.updateEvent(e, -1, 1, key);
+      }
+    });
+
+    D.body.addEventListener('mouseup', (e) => {
+      let key = e.button + 1;
+      if (key in this.events) {
+        this.updateEvent(e, 1, 0, key);
       }
     });
   }
@@ -31,6 +41,17 @@ class Input {
     }
   }
 
+  isLeftClick() {
+    // event.button (0) + 1
+    return this.isPressed(1);
+  }
+
+  isRightClick() {
+    // event.button (2) + 1
+    return this.isPressed(3);
+  }
+
+  // TODO: Unbind
   bind(keys) {
     keys.forEach((k) => {
       this.pressed[k] = 0;
@@ -38,5 +59,20 @@ class Input {
     });
   }
 
-  // TODO: Unbind
+  // We use event.button + 1 to bind mouse input for left and right clicks
+  // to use the ones coming from js and avoid defining new variables. Plus,
+  // 1 and 3 are codes for non-printable characters, so there is no chance
+  // of key collision.
+  // 1: left click
+  // 3: right click
+  bindMouse() {
+    this.bind([1, 3]);
+  }
+
+  updateEvent(e, m, n, key) {
+    key = key || e.keyCode;
+    e.preventDefault();
+    this.events[key] = m;
+    this.pressed[key] = n;
+  }
 }
